20007df9c34f2acbbfc9f853fc815a4d4885cb1f
Jonatas <jonatasmacedo7)@hotmail.com>
2020-02-09 22:07:21 -0300

    corrige app.json com entrada para android e componente caso não tenha o atributo end disponivel e adiciona assets

--- src/component/ScheduleFlatList.js
+++ src/component/ScheduleFlatList.js
@@ -1,24 +1,34 @@
 import React, { Component, useEffect, useState } from 'react';
 import { FlatList } from 'react-native';
 import ScheduleFrame from './ScheduleFrame';
+import ScheduleFrameHalf from './ScheduleFrameHalf';
 
 class CustomFlatList extends Component {
     render() {
         const sche = this.props.data;
         const scheduleData = sche.schedules.map(
-            (sch) => (
-                {
+            (sch) => {
+                if (sch.end) {
+                    return {
+                        key: sch.type,
+                        end: {
+                            hours: sch.end.hours.join(', '),
+                            description: sch.end.description
+                        },
+                        start: {
+                            hours: sch.start.hours.join(', '),
+                            description: sch.start.description
+                        }
+                    };
+                }
+                return {
                     key: sch.type,
-                    end: {
-                        hours: sch.end.hours.join(', '),
-                        description: sch.end.description
-                    },
                     start: {
                         hours: sch.start.hours.join(', '),
                         description: sch.start.description
                     }
-                }
-            )
+                };
+            }
         );
 
         return (
@@ -26,9 +36,12 @@ class CustomFlatList extends Component {
             style={{marginBottom: 50,}}
                 data={scheduleData}
                 renderItem={
-                    ({ item }) => (
-                        <ScheduleFrame type={item.key} end={item.end} start={item.start}/>
-                    )
+                    ({ item }) => {
+                        if (item.end) {
+                            return (<ScheduleFrame type={item.key} end={item.end} start={item.start}/>)
+                        }
+                        return (<ScheduleFrameHalf type={item.key} start={item.start}/>)
+                    }
                 }
             />
         );
